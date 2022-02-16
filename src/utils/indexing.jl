160f074f224a7aed7d866ab86114cae361bb3175
Miller, Samuel <smil@lle.rochester.edu>
2022-02-16 14:36:10 -0500

    Updates to indexing functions + unit tests  - These find the halo, domain, and domain donor (to halo) indices

--- src/utils/indexing.jl
+++ src/utils/indexing.jl
@@ -12,24 +12,34 @@ Helper functions to get the low side halo and domain starting/ending indices
     lo_indices = first(CI) |> Tuple
     hi_indices = last(CI) |> Tuple
     
-    lo_halo_start = lo_indices[dim]        # start index of the halo region
+    lo_halo_start = lo_indices[dim] # start index of the halo region
+    lo_halo_end = lo_halo_start + nhalo - 1
 
-    lo_dom_start = nhalo + 1 # start index of the inner domain
-    lo_dom_end = 2 * nhalo   # end index of the inner domain
-    lo_halo_end = lo_halo_start + nhalo - 1 # end index of the halo region
+    lo_dom_start = lo_halo_end + 1 # start index of the inner domain
+    lo_dom_end = lo_dom_start + nhalo - 1       # end index of the inner domain
 
     return (lo_halo_start, lo_halo_end, lo_dom_start, lo_dom_end)
+end
+
+@inline function lo_indices(A::MPIHaloArray{T,N}, dim) where {T,N}
+    lo_indices(A.data, dim, A.nhalo)
 end
 
 """Helper functions to get the high side halo and domain starting/ending indices"""
 @inline function hi_indices(field, dim, nhalo)
 
-    if dim > length(size(field)) error("dim > length(size(field))") end
-    hi_halo_end = size(field, dim)       # end index of the halo region
+    CI = CartesianIndices(field)
+    hi_indices = last(CI) |> Tuple
+
+    hi_halo_end = hi_indices[dim]            # end index of the halo region
     hi_halo_start = hi_halo_end - nhalo + 1  # start index of the halo region
 
     hi_dom_end = hi_halo_start - 1   # end index of the inner domain
     hi_dom_start = hi_dom_end - nhalo + 1 # start index of the inner domain
 
     return (hi_dom_start, hi_dom_end, hi_halo_start, hi_halo_end)
+end
+
+@inline function hi_indices(A::MPIHaloArray{T,N}, dim) where {T,N}
+    hi_indices(A.data, dim, A.nhalo)
 end
