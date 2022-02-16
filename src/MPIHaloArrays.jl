a05e7b22f133e53991a49eda1eded4db299123f5
Miller, Samuel <smil@lle.rochester.edu>
2022-02-16 14:34:26 -0500

    Added DataIndices that keep track of local/global indices within each MPIHaloArray

--- src/MPIHaloArrays.jl
+++ src/MPIHaloArrays.jl
@@ -8,7 +8,18 @@ using .ParallelTopologies
 
 export MPIHaloArray
 export ParallelTopology, CartesianTopology
-export neighbor, neighbors, ilo_neighbor, ihi_neighbor, jlo_neighbor, jhi_neighbor, klo_neighbor, khi_neighbor
+export neighbor, neighbors
+export ilo_neighbor, ihi_neighbor, jlo_neighbor, jhi_neighbor, klo_neighbor, khi_neighbor
+export lo_indices, hi_indices
+export sync_edges!
+
+struct DataIndices{T <: Integer}
+    lo_halo::NTuple{2,T}               # (start,end) indices of the halo region on the low side
+    lo_halo_domain_donor::NTuple{2,T}  # (start,end) indices of the domain region on the low side that "donates" to the halo region
+    domain::NTuple{2,T}                # (start,end) indices of the real domain
+    hi_halo_domain_donor::NTuple{2,T}  # (start,end) indices of the domain region on the high side that "donates" to the halo region
+    hi_halo::NTuple{2,T}               # (start,end) indices of the halo region on the high side
+end
 
 """
 MPIHaloArray
@@ -23,22 +34,91 @@ MPIHaloArray
  - `rank`: Current MPI rank
 """
 mutable struct MPIHaloArray{T,N} <: AbstractArray{T,N}
-    data::Array{T,N}
+    data::AbstractArray{T,N}
     nhalo::Int
     rank::Int
     topology::CartesianTopology
-    # comm::MPI.Comm
-    # window::MPI.Win
-    # neighbor_ranks::Vector{Int}
-    # coords::Vector{Int}
+    local_indices::Vector{DataIndices}
+    global_indices::Vector{DataIndices}
     # MPIHaloArray{T}(sizes::Vararg{<:Integer,N}, nhalo) where {T,N} = MPIHaloArray(Array{T, 2}(undef, sizes...), nhalo, 0...)
 end
 
-function MPIHaloArray(A::Array{T,N}, topo::CartesianTopology, nhalo::Int) where {T,N}
-    if topo.rank == 0
-        @show topo
+function MPIHaloArray(A::AbstractArray{T,N}, topo::CartesianTopology, nhalo::Int) where {T,N}
+    local_di = Vector{DataIndices}(undef, N)
+    global_di = Vector{DataIndices}(undef, N)
+
+    A_with_halo = pad_with_halo(A, nhalo)
+
+    for dim in 1:N
+        lo_halo_start, lo_halo_end, lo_dom_start, lo_dom_end = lo_indices(A_with_halo, dim, nhalo)
+        hi_dom_start, hi_dom_end, hi_halo_start, hi_halo_end = hi_indices(A_with_halo, dim, nhalo)
+
+        local_di[dim] = DataIndices((lo_halo_start, lo_halo_end), 
+                                    (lo_dom_start , lo_dom_end),
+                                    (lo_dom_start , hi_dom_end),
+                                    (hi_dom_start , hi_dom_end),
+                                    (hi_halo_start, hi_halo_end))
+        
+        global_offset = (hi_halo_end - lo_halo_start + 1) * topo.coords[dim]
+
+        global_di[dim] = DataIndices((lo_halo_start, lo_halo_end) .+ global_offset, 
+                                     (lo_dom_start , lo_dom_end ) .+ global_offset,
+                                     (lo_dom_start , hi_dom_end ) .+ global_offset,
+                                     (hi_dom_start , hi_dom_end ) .+ global_offset,
+                                     (hi_halo_start, hi_halo_end) .+ global_offset)
     end
-    MPIHaloArray(A, nhalo, topo.rank, topo)
+
+    update_halo_data!(A, A_with_halo, local_di)
+    MPIHaloArray(A_with_halo, nhalo, topo.rank, topo, local_di, global_di)
+end
+
+
+function update_halo_data!(A_no_halo::AbstractArray{T,1}, A_with_halo::AbstractArray{T,1}, local_data_indices) where {T}
+    ilo_dom, ihi_dom = local_data_indices[1].domain
+    ilo, ihi = firstindex(A_no_halo), lastindex(A_no_halo)
+    for (i_nh, i_wh) in zip(ilo:ihi, ilo_dom:ihi_dom)
+        A_with_halo[i_wh] = A_no_halo[i_nh]
+    end
+end
+
+function update_halo_data!(A_no_halo::AbstractArray{T,2}, A_with_halo::AbstractArray{T,2}, local_data_indices) where {T}
+    ilo_dom, ihi_dom = local_data_indices[1].domain
+    jlo_dom, jhi_dom = local_data_indices[2].domain
+
+    CI = CartesianIndices(A_no_halo)
+    ilo, jlo = first(CI) |> Tuple
+    ihi, jhi = last(CI) |> Tuple
+
+    for (j_nh, j_wh) in zip(jlo:jhi, jlo_dom:jhi_dom)
+        for (i_nh, i_wh) in zip(ilo:ihi, ilo_dom:ihi_dom)
+            A_with_halo[i_wh, j_wh] = A_no_halo[i_nh, j_nh]
+        end
+    end
+end
+
+function update_halo_data!(A_no_halo::AbstractArray{T,3}, A_with_halo::AbstractArray{T,3}, local_data_indices) where {T}
+    ilo_dom, ihi_dom = local_data_indices[1].domain
+    jlo_dom, jhi_dom = local_data_indices[2].domain
+    klo_dom, khi_dom = local_data_indices[3].domain
+
+    CI = CartesianIndices(A_no_halo)
+    ilo, jlo, klo = first(CI) |> Tuple
+    ihi, jhi, khi = last(CI) |> Tuple
+
+    for (k_nh, k_wh) in zip(klo:khi, klo_dom:khi_dom)
+        for (j_nh, j_wh) in zip(jlo:jhi, jlo_dom:jhi_dom)
+            for (i_nh, i_wh) in zip(ilo:ihi, ilo_dom:ihi_dom)
+                A_with_halo[i_wh, j_wh, k_wh] = A_no_halo[i_nh, j_nh, k_nh]
+            end
+        end
+    end
+end
+
+function pad_with_halo(A,nhalo)
+    new_dims = size(A) .+ 2nhalo
+    A_new = similar(A, new_dims)
+    fill!(A_new, zero(eltype(A)))
+    return A_new
 end
 
 # Required interface overloads to be an AbstractArray
@@ -48,13 +128,11 @@ Base.getindex(A::MPIHaloArray{T,N}, I::Vararg{Int, N}) where {T,N} = getindex(A.
 Base.setindex!(A::MPIHaloArray{T,N}, v, i::Int) where {T,N} = setindex!(A.data, v, i)
 Base.setindex!(A::MPIHaloArray{T,N}, v, I::Vararg{Int, N}) where {T,N} = setindex!(A.data, v..., I...)
 
+include("utils/indexing.jl")
 
 
-# include("utils/indexing.jl")
-
-# include("sync_edges.jl")
+include("sync_edges.jl")
 # include("ops.jl")
-
 
 # """
 #     applylocalfunc(f, A::MPIHaloArray)
@@ -67,76 +145,4 @@ Base.setindex!(A::MPIHaloArray{T,N}, v, I::Vararg{Int, N}) where {T,N} = setinde
 #     return result
 # end
 
-# """
-#     applylocalfunc(f, A::MPIHaloArray)
-# Execute the function f on the part of a owned by the current rank. The local part may be modified by f.
-# """
-# function applylocalfunc!(f, A::MPIHaloArray)
-#     MPI.Win_lock(MPI.LOCK_EXCLUSIVE, A.rank, 0, A.window)
-#     result = f(A.data)
-#     MPI.Win_unlock(A.rank, A.window)
-#     return result
-# end
-
-
-# function init_global_grid(nx::Integer, ny::Integer, nz::Integer; 
-#                           dimx::Integer=0, dimy::Integer=0, dimz::Integer=0, nhalo=2,
-#                           periodx::Integer=0, periody::Integer=0, periodz::Integer=0, 
-#                           disp::Integer=1, reorder::Integer=1, comm::MPI.Comm=MPI.COMM_WORLD, init_MPI::Bool=true, quiet::Bool=false)
-
-#     nxyz          = [nx, ny, nz];
-#     dims          = [dimx, dimy, dimz];
-#     periods       = [periodx, periody, periodz];
-
-#     dims[(nxyz .== 1) .& (dims .== 0)] .= 1;   # Setting any of nxyz to 1, means that the corresponding dimension must also be 1 in the global grid. Thus, the corresponding dims entry must be 1.
-    
-#     if (init_MPI)  # NOTE: init MPI only, once the input arguments have been checked.
-#         if (MPI.Initialized()) error("MPI is already initialized. Set the argument 'init_MPI=false'."); end
-#         MPI.Init();
-#     else
-#         if (!MPI.Initialized()) error("MPI has not been initialized beforehand. Remove the argument 'init_MPI=false'."); end  # Ensure that MPI is always initialized after init_global_grid().
-#     end
-
-#     nprocs    = MPI.Comm_size(comm);
-#     MPI.Dims_create!(nprocs, dims);
-#     comm_cart = MPI.Cart_create(comm, dims, periods, reorder);
-#     me        = MPI.Comm_rank(comm_cart);
-#     coords    = MPI.Cart_coords(comm_cart);
-#     neighbors = fill(MPI.MPI_PROC_NULL, NNEIGHBORS_PER_DIM, NDIMS_MPI);
-#     for i = 1:NDIMS_MPI
-#         neighbors[:,i] .= MPI.Cart_shift(comm_cart, i - 1, disp);
-#     end
-#     return me, neighbors, dims, nprocs, coords, comm_cart # The typical use case requires only these variables; the remaining can be obtained calling get_global_grid() if needed.
-# end
-
-# # function print_arr(U)
-# #     for proc in 0:nprocs
-# #         if me == proc
-# #             println()
-# #             println("proc: ", proc)
-# #             for j in size(U, 2):-1:1
-# #                 println("j ", j, ":\t", U[:,j])
-# #             end
-# #         end
-# #         MPI.Barrier(comm_cart)
-# #     end
-# # end
-
-# # ni = 8
-# # nj = 10
-# # nhalo = 2
-# # me, neighbors, dims, nprocs, coords, comm_cart = init_global_grid(ni, nj, 1)
-
-# # ilo_neighbor, jlo_neighbor, klo_neighbor = neighbors[1,:]
-# # ihi_neighbor, jhi_neighbor, khi_neighbor = neighbors[2,:]
-
-# # U = zeros(ni, nj)
-# # U .= me
-
-# # print_arr(U)
-
-# # println("After")
-# # print_arr(U)
-
-# # MPI.Finalize()
 end
